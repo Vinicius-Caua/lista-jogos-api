cbc4c0868d4946ff39d9a8b95185f28c2c090239
Vinicius Caua <vinipg1@hotmail.com>
2024-11-11 17:24:43 -0300

    Alteração README

---
+++ README.md
@@ -0,0 +1,3 @@
+## Lista Tarefas Spring Boot
+
+Projeto teste de uma lista de jogos, usando Spring Boot
--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -30,8 +30,6 @@ public class Game {
     public Game() {
     }
 
-    
-
     public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
             String shortDescription, String longDescription) {
         this.id = id;
